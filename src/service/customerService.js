37ed8d172df19c57a1c9ecc96871df5e38c7d246
payam rastogi <payamrastogi@outlook.com>
2024-06-05 16:54:51 -0400

    UI: Customer Page - Add a snackbar to display errors related to the api call #29

--- src/service/customerService.js
+++ src/service/customerService.js
@@ -13,34 +13,21 @@ export function createCustomer(customerDetails) {
 }
 
 export function getCustomers() {
-  const response = axios
-    .get("http://192.168.1.44:8002/api/customer/getAll")
-    .catch((error) => {
-      console.log(error);
-      return undefined;
-    });
+  const response = axios.get("http://192.168.1.44:8003/api/customer/getAll");
   return response;
 }
 
 export function getCustomerById(id) {
-  const response = axios
-    .get(`http://192.168.1.44:8002/api/customer/getById/${id}`)
-    .catch((error) => {
-      console.log(error);
-      return undefined;
-    });
+  const response = axios.get(
+    `http://192.168.1.44:8002/api/customer/getById/${id}`
+  );
   return response;
 }
 
 export function updateCustomer(customerDetails) {
-  const response = axios
-    .put(
-      `http://192.168.1.44:8002/api/customer/update/${customerDetails.id}`,
-      customerDetails
-    )
-    .catch((error) => {
-      console.log(error);
-      return undefined;
-    });
+  const response = axios.put(
+    `http://192.168.1.44:8002/api/customer/update/${customerDetails.id}`,
+    customerDetails
+  );
   return response;
 }
