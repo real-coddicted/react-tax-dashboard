5ccf4f2e3e752b80fb88ee2cebad6764076a4838
payam rastogi <payamrastogi@outlook.com>
2024-06-17 13:25:05 -0400

    UI: Customer Add Page #Stepper - Add a step to select tax services for a customer #32

--- src/scenes/form/tax/Tax.jsx
+++ src/scenes/form/tax/Tax.jsx
@@ -12,44 +12,6 @@ import ESIC from "./ESIC";
 import TDS from "./TDS";
 import FileUpload from "./FileUpload";
 
-const services = {
-  incomeTax: {
-    label: "Income Tax",
-    enable: true,
-    index: 0,
-  },
-  gst: {
-    label: "GST",
-    enable: true,
-    index: 1,
-  },
-  mca: {
-    label: "MCA",
-    enable: true,
-    index: 2,
-  },
-  pf: {
-    label: "PF",
-    enable: true,
-    index: 3,
-  },
-  esic: {
-    label: "ESIC",
-    enable: true,
-    index: 4,
-  },
-  tds: {
-    label: "TDS",
-    enable: true,
-    index: 5,
-  },
-  documents: {
-    label: "Documents",
-    enable: true,
-    index: 6,
-  },
-};
-
 function CustomTabPanel(props) {
   const { children, value, index, ...other } = props;
 
@@ -95,20 +57,43 @@ export default function Tax(props) {
           value={value}
           onChange={handleChange}
           aria-label="services tabs"
+          sx={{
+            "& .Mui-disabled": {
+              color: "black",
+            },
+          }}
         >
-          {/* <Tab label="Income Tax" {...a11yProps(0)} />
-          <Tab label="GST" {...a11yProps(1)} />
-          <Tab label="MCA" {...a11yProps(2)} />
-          <Tab label="PF" {...a11yProps(3)} />
-          <Tab label="ESIC" {...a11yProps(4)} />
-          <Tab label="TDS" {...a11yProps(5)} />
-          <Tab label="Documents" {...a11yProps(6)} /> */}
-          {Object.entries(services).forEach(([key, value]) => {
-            console.log(
-              value.label + "**" + value.enable + "***" + value.index
-            );
-            return <Tab label={value.label} {...a11yProps(value.index)} />;
-          })}
+          <Tab
+            label="Income Tax"
+            {...a11yProps(0)}
+            disabled={!props.data.serviceAvailed.incomeTax}
+          />
+          <Tab
+            label="GST"
+            {...a11yProps(1)}
+            disabled={!props.data.serviceAvailed.gst}
+          />
+          <Tab
+            label="MCA"
+            {...a11yProps(2)}
+            disabled={!props.data.serviceAvailed.mca}
+          />
+          <Tab
+            label="PF"
+            {...a11yProps(3)}
+            disabled={!props.data.serviceAvailed.pf}
+          />
+          <Tab
+            label="ESIC"
+            {...a11yProps(4)}
+            disabled={!props.data.serviceAvailed.esic}
+          />
+          <Tab
+            label="TDS"
+            {...a11yProps(5)}
+            disabled={!props.data.serviceAvailed.tds}
+          />
+          <Tab label="Documents" {...a11yProps(6)} />
         </Tabs>
       </Box>
       <CustomTabPanel value={value} index={0}>
