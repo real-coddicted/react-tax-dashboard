a471e0a5c015611924fd9c20e5353a5455e33d15
payam rastogi <payamrastogi@outlook.com>
2024-08-03 22:33:54 -0400

    UI: Enable search capability on the home page #10

--- src/service/customerService.js
+++ src/service/customerService.js
@@ -32,7 +32,18 @@ export function updateCustomer(customerDetails) {
 
 export function deleteCustomerById(id) {
   const response = axios.delete(
-    `${process.env.REACT_APP_API_URL}/api/customer/remove/${id}22`
+    `${process.env.REACT_APP_API_URL}/api/customer/remove/${id}`
+  );
+  return response;
+}
+
+export function searchCustomers(searchText) {
+  const request = {
+    textToSearch: searchText,
+  };
+  const response = axios.post(
+    `${process.env.REACT_APP_API_URL}/api/customer/search`,
+    request
   );
   return response;
 }
